b541ab64f887c3e01ae7e30f74a2ac46bfca4ab3
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2022-10-13 15:27:55 +0600

    Update TestController.java

--- src/main/java/com/ecommerce/springBootEcommerce/controller/TestController.java
+++ src/main/java/com/ecommerce/springBootEcommerce/controller/TestController.java
@@ -1,5 +1,4 @@
-package com.login.security.controllers;
-
+package com.ecommerce.springBootEcommerce.controller;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
